6a52ab13d98138fa09e4a803e53b4e28eead5ede
irfan.khan@reddotdigitalit.com_ITP_50000142 <irfan.khan@reddotdigitalit.com>
2023-08-09 16:29:50 +0600

    Update LeetCode

--- src/main/java/com/topic/LearningApplication.java
+++ src/main/java/com/topic/LearningApplication.java
@@ -1,7 +1,6 @@
 package com.topic;
 
-import com.topic.leetcode.BestTimeTOBuyAndSellStock;
-import com.topic.leetcode.ContainsDuplicate;
+import com.topic.leetcode.ProductOfArrayExceptSelf;
 import com.topic.override.ChildClass;
 import com.topic.override.ParentClass;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +17,12 @@ public class LearningApplication {
         objectC.isFunction();
         objectP.isFunction();
 
+        callLeetCodeFunction();
+
+    }
+
+    public static void callLeetCodeFunction() {
+
 //        TwoSum twoSum = new TwoSum();
 //        int[] a = twoSum.twoSum(new int[]{1, 2, 3, 4, 5}, 6);
 //        for (int b : a) {
@@ -25,12 +30,18 @@ public class LearningApplication {
 //        }
 
 //        BestTimeTOBuyAndSellStock temp = new BestTimeTOBuyAndSellStock();
-//        int a = temp.maxProfit(new int[]{7,1,5,3,6,4});
+//        int a = temp.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
 //        System.out.println(a);
 
-        ContainsDuplicate temp = new ContainsDuplicate();
-        boolean a = temp.containsDuplicate(new int[]{1,2,3,4});
-        System.out.println(a);
+//        ContainsDuplicate temp = new ContainsDuplicate();
+//        boolean a = temp.containsDuplicate(new int[]{1, 2, 3, 4});
+//        System.out.println(a);
+
+        ProductOfArrayExceptSelf temp = new ProductOfArrayExceptSelf();
+        int[] a = temp.productExceptSelf(new int[]{1, 2, 3, 4});
+        for (int b : a) {
+            System.out.println(b);
+        }
     }
 
 }
